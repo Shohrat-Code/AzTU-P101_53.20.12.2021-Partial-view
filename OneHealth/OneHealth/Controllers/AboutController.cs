7a184008fd480977bb49dfdf396487d1e142ea8f
Shohrat-Code <shohrat@code.edu.az>
2021-12-20 13:05:07 +0400

    Codes

---
+++ OneHealth/OneHealth/Controllers/AboutController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OneHealth.Data;
+using OneHealth.Models;
+using OneHealth.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneHealth.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AboutController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            VmHome model = new VmHome();
+            model.Setting = _context.Settings.FirstOrDefault();
+            model.Socials = _context.Socials.ToList();
+            return View(model);
+        }
+    }
+}
---
+++ OneHealth/OneHealth/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using OneHealth.Data;
+using OneHealth.Models;
+using OneHealth.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneHealth.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            VmHome model = new VmHome();
+            model.Setting= _context.Settings.FirstOrDefault();
+            model.Socials = _context.Socials.ToList();
+
+            model.Services = _context.Services.ToList();
+            model.Doctors = _context.Doctors.Include("Specialty").ToList();
+            model.Blogs = _context.Blogs.Include("User")
+                                        .Include("Category")
+                                        .OrderByDescending(o => o.CreatedDate)
+                                        .Take(3).ToList();
+            return View(model);
+        }
+
+    }
+}
